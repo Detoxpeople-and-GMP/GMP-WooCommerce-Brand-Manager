e2017bcfb3f2cc7f7a3ed52d5c60a7807c8f430c
Shalom Taiwo <ifeshalomt@gmail.com>
2025-03-12 08:45:45 +0200

    Update author information in plugin header

--- gmp-woocommerce-brand-manager.php
+++ gmp-woocommerce-brand-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: GMP WooCommerce Brand Manager
 Description: A plugin to copy or move products between brands.
 Version: 1.0.0
-Author: GMP (ShalomT)
+Author: GMP
 */
 
 if (!defined('ABSPATH')) {
